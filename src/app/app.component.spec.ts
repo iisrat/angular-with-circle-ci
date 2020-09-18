0ea4b39c22704ffd55c950d5cd4841dae7be1049
isratzaman <israt.zaman@welldev.io>
2020-09-18 22:43:30 +0600

    fixed test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'practice-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('practice-app');
+    expect(app.title).toEqual('This is my practice-app!!!');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('practice-app app is running!');
+    expect(compiled.querySelector('h1').innerHTML).toContain('This is my practice-app!!!');
   });
 });
